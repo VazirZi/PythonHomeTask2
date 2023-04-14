ff23b9ce79c22b59f2b77a1366d0e3db6f69912d
VazirZi <vazir.zivar@mail.ru>
2023-04-14 16:31:17 +0300

    Add Unit 2

--- Main.py
+++ Main.py
@@ -19,3 +19,16 @@ if emblems > tails:
     print(f'нужно первернуть {tails} монет.')
 else:
     print(f'Нужно перевернуть {emblems} монет.')
+
+# Unit 2
+
+firstDigit = int(input('Петя загадывает первое число: '))
+secondDigit = int(input('Петя загадывает второе число: '))
+
+sumOfDigits = firstDigit + secondDigit
+productOfDigits = firstDigit * secondDigit
+
+for i in range(1001):
+    for j in range(1001):
+        if i + j == sumOfDigits and i * j == productOfDigits:
+            print(f'Катя говорит, что загаданные числа: {i} и {j}')
